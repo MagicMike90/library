d63b471b7b3163658c6f79ca09e3a409c0324108
MagicMike90 <lcw303661398@hotmail.com>
2017-11-07 22:05:49 +1100

    update express

--- server/config/express.js
+++ server/config/express.js
@@ -50,23 +50,24 @@ app.use(function (req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+    const msg = {
+      'error': {
+        message: err.message,
+        error: err
+      }
+    }
+    res.status(err.status || 500).json(msg);
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function (err, req, res, next) {
+//   res.status(err.status || 500).json('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
 
 
 module.exports = app;
