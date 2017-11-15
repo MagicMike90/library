6c55bf31a87c262c40a7e75ed5376e17b01dd835
Michael Luo <mluo@echoice.com.au>
2017-11-16 09:56:59 +1100

    update project to angular 5

--- src/app/auth/components/login-form/login-form.component.ts
+++ src/app/auth/components/login-form/login-form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
-
+import 'rxjs/add/operator/delay';
 
 import { Authenticate } from '../../models/user';
 import * as fromAuth from '../../reducers';
