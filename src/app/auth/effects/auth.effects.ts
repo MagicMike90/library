25cb479517186dc414d3b29664d4c8c2ff091f9a
Michael Luo <michael.luo0801@gmail.com>
2019-02-26 23:19:21 +1100

    test auth actions

--- src/app/auth/effects/auth.effects.ts
+++ src/app/auth/effects/auth.effects.ts
@@ -1,12 +1,12 @@
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { Actions, Effect, ofType } from '@ngrx/effects';
-import { of } from 'rxjs';
-import { exhaustMap, map, tap, catchError } from 'rxjs/operators';
-import * as Auth from '../actions/auth';
-import * as Registration from '../actions/registration';
-import AuthStore from '../auth.store';
-import { AuthService } from '../services/auth.service';
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { Actions, Effect, ofType } from "@ngrx/effects";
+import { of } from "rxjs";
+import { exhaustMap, map, tap, catchError } from "rxjs/operators";
+import * as Auth from "../actions/auth";
+import * as Registration from "../actions/registration";
+import AuthStore from "../auth.store";
+import { AuthService } from "../services/auth.service";
 
 @Injectable()
 export class AuthEffects {
@@ -28,21 +28,24 @@ export class AuthEffects {
   @Effect({ dispatch: false })
   loginSuccess = this.actions.pipe(
     ofType(Auth.LOGIN_SUCCESS),
-    tap(() => this.router.navigate(['/app/dashboard']))
+    tap(() => this.router.navigate(["/app/dashboard"]))
   );
 
   @Effect({ dispatch: false })
   loginRedirect = this.actions.pipe(
     ofType(Auth.LOGIN_REDIRECT, Auth.LOGOUT),
     tap(authed => {
-      this.router.navigate(['/auth/login']);
+      this.router.navigate(["/auth/login"]);
     })
   );
 
   @Effect()
   register = this.actions.pipe(
     ofType(Registration.REGISTER),
-    map((action: Registration.Signup) => action.payload),
+    map((action: Registration.Signup) => {
+      console.log("action.payload", action.payload);
+      return action.payload;
+    }),
     exhaustMap(auth =>
       this.authService
         .register(auth)
@@ -58,7 +61,7 @@ export class AuthEffects {
   registerSuccess = this.actions.pipe(
     ofType(Registration.REGISTER_SUCCESS),
     tap(authed => {
-      this.router.navigate(['/app']);
+      this.router.navigate(["/app"]);
     })
   );
 
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,26 +1,27 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { Authenticate } from '../models/user';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { Authenticate } from "../models/user";
 
 @Injectable()
 export class AuthService {
   private headers: HttpHeaders;
-  private apiEndpoint = 'http://localhost:5000/auth'; // URL to web api
+  private apiEndpoint = "http://localhost:5000/auth"; // URL to web api
 
   constructor(private http: HttpClient) {
     this.headers = new HttpHeaders();
-    this.headers = this.headers.set('Content-Type', 'application/json');
-    this.headers = this.headers.set('Application-Names', ['store', 'auth']);
+    this.headers = this.headers.set("Content-Type", "application/json");
+    this.headers = this.headers.set("Application-Names", ["store", "auth"]);
   }
 
   register(user: Authenticate): any {
-    const signInUrl = this.apiEndpoint + '/signup';
+    console.log("register", user);
+    const signInUrl = this.apiEndpoint + "/signup";
     return this.http.post(signInUrl, user, { headers: this.headers });
   }
 
   login(user: Authenticate): Observable<any> {
-    const signInUrl = this.apiEndpoint + '/signin';
+    const signInUrl = this.apiEndpoint + "/signin";
     return this.http.post(signInUrl, user, { headers: this.headers });
   }
 
